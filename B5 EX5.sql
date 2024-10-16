6b3ca1b4e57201fe989a439217b8dffdb00b329c
NgocHan-code <hantran.31231025577@st.ueh.edu.vn>
2024-10-16 07:49:59 +0700

    Update B5 EX5.sql

--- B5 EX5.sql
+++ B5 EX5.sql
@@ -1,5 +1,6 @@
-SELECT user_id, EXTRACT(DAY FROM (MAX(post_date) - MIN(post_date))) AS "days_between"
-FROM posts
-WHERE post_date BETWEEN '2021-01-01' AND '2021-12-31'
-GROUP BY user_id
-HAVING count(post_id) > 1;
+SELECT candidate_id FROM candidates WHERE skill = 'Python'
+INTERSECT 
+SELECT candidate_id FROM candidates WHERE skill = 'Tableau'
+INTERSECT  
+SELECT candidate_id FROM candidates WHERE skill = 'PostgreSQL'
+ORDER BY 1;
