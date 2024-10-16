926fac1665b1ca5baf4783fac3451bce463e2b2e
NgocHan-code <hantran.31231025577@st.ueh.edu.vn>
2024-10-16 07:51:52 +0700

    Update B5 EX7.sql

--- B5 EX7.sql
+++ B5 EX7.sql
@@ -1,3 +1,5 @@
-select name 
-from Employee
-Order by name asc;
+SELECT card_name,
+max(issued_amount)-min(issued_amount)as difference
+from monthly_cards_issued
+group by card_name
+order by difference desc;
